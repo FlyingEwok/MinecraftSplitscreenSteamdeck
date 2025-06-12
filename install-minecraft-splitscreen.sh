2ac56141a2d224dd2d12a58745253e5ef2a5bb7a
FlyingEwok <nicholas.garth1@gmail.com>
2025-06-12 03:26:54 -0400

    Update Fabric loader version in installer script to 0.16.14 for improved compatibility

--- install-minecraft-splitscreen.sh
+++ install-minecraft-splitscreen.sh
@@ -116,8 +116,8 @@ ________EOF
                 cp "$mod" ".minecraft/mods/"
             done
 
-            # Use Fabric installer to set up loader in this instance
-            "$JAVA_PATH" -jar "$TEMP_DIR/fabric-installer-0.11.2.jar" client -dir ".minecraft" -mcversion 1.21.5 -loader 0.15.7 -noprofile
+            # Use Fabric installer to set up loader in this instance (latest: 0.16.14)
+            "$JAVA_PATH" -jar "$TEMP_DIR/fabric-installer-0.11.2.jar" client -dir ".minecraft" -mcversion 1.21.5 -loader 0.16.14 -noprofile
 
             # Copy Minecraft jar and json
             cp "$TEMP_DIR/1.21.5.jar" ".minecraft/versions/1.21.5/1.21.5.jar"
